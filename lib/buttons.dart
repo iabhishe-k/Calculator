cc8624616450b6665b8dcc94c92ac7dfbe9b3b36
Abhishek Kumar Singh <abhishek99.aks@gmail.com>
2024-01-11 19:03:45 +0530

    first commit

---
+++ lib/buttons.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  const MyButton({
+    super.key,
+    required this.color,
+    required this.textColor,
+    required this.buttonText,
+    required this.buttonTapped,
+  });
+
+  final color;
+  final textColor;
+  final String buttonText;
+  final buttonTapped;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: buttonTapped,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Container(
+            color: color,
+            child: Center(
+              child: Text(
+                buttonText,
+                style: TextStyle(
+                  color: textColor,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,172 @@
+import 'package:calculator/buttons.dart';
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var userQuestion = "";
+  var userAnswer = "";
+  var equal = false;
+  var nextQuestion = "";
+
+  final List<String> buttons = [
+    'C',
+    'DEL',
+    '%',
+    '/',
+    '7',
+    '8',
+    '9',
+    'x',
+    '4',
+    '5',
+    '6',
+    '-',
+    '1',
+    '2',
+    '3',
+    '+',
+    '00',
+    '0',
+    '.',
+    '=',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[50],
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  const SizedBox(height: 120),
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    alignment: Alignment.centerRight,
+                    child: Text(userQuestion,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        )),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      userAnswer,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              child: GridView.builder(
+                itemCount: buttons.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4),
+                itemBuilder: (BuildContext context, int index) {
+                  return MyButton(
+                    buttonTapped: () {
+                      setState(
+                        () {
+                          if (index == 1) {
+                            userQuestion = userQuestion.substring(
+                                0, userQuestion.length - 1);
+                          } else if (index == 0) {
+                            userQuestion = '';
+                            userAnswer = '';
+                          } else if (index == buttons.length - 1) {
+                            equalPressed();
+                          } else {
+                            if (equal == true) {
+                              userQuestion = nextQuestion;
+                              equal = false;
+                              userAnswer = "";
+                            }
+                            userQuestion += buttons[index];
+                          }
+                        },
+                      );
+                    },
+                    color: isOperator(buttons[index])
+                        ? Colors.deepPurple
+                        : Colors.deepPurple[300],
+                    textColor: Colors.white,
+                    buttonText: buttons[index],
+                  );
+                },
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  bool isOperator(String x) {
+    if (x == 'C' ||
+        x == '%' ||
+        x == 'DEL' ||
+        x == '/' ||
+        x == 'x' ||
+        x == '-' ||
+        x == '+' ||
+        x == '=') {
+      return true;
+    }
+    return false;
+  }
+
+  void equalPressed() {
+    String finalQuestion = userQuestion;
+    finalQuestion = finalQuestion.replaceAll('x', '*');
+    Parser p = Parser();
+    Expression exp = p.parse(finalQuestion);
+    ContextModel cm = ContextModel();
+    double eval = exp.evaluate(EvaluationType.REAL, cm);
+
+    int intEval = eval.toInt();
+    if (eval - intEval == 0) {
+      userAnswer = intEval.toString();
+    } else {
+      userAnswer = eval.toString();
+    }
+    nextQuestion = userAnswer;
+    equal = true;
+  }
+}
